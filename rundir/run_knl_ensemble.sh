67fb19c6f493bc5a2b52df47f17a1a9144b82c66
Alfredo Gimenez <gimenez1@llnl.gov>
2018-03-23 12:01:41 -0700

    Better ensemble script

--- rundir/run_knl_ensemble.sh
+++ rundir/run_knl_ensemble.sh
@@ -3,29 +3,25 @@
 function xsrun () {
     RUN="s_$1-l_$2"
 
-    CALI_CONFIG_PROFILE=load-counts-knl \
-        CALI_RECORDER_FILENAME=counts-${RUN}-NoHBM.cali \
-        CALI_REPORT_FILENAME=counts-${RUN}-NoHBM.json \
+    CALI_RECORDER_FILENAME=${CALI_CONFIG_PROFILE}-${RUN}-NoHBM.cali \
         ./XSBench -s $1 -l $2 
 
-    CALI_CONFIG_PROFILE=load-samples-knl \
-        CALI_RECORDER_FILENAME=samples-${RUN}-NoHBM.cali \
-        CALI_REPORT_FILENAME=samples-${RUN}-NoHBM.json \
-        ./XSBench -s $1 -l $2 
-
-    CALI_CONFIG_PROFILE=load-counts-knl \
-        CALI_RECORDER_FILENAME=counts-${RUN}-FullHBM.cali \
-        CALI_REPORT_FILENAME=counts-${RUN}-FullHBM.json \
-        numactl --membind=1 ./XSBench -s $1 -l $2 
-
-    CALI_CONFIG_PROFILE=load-samples-knl \
-        CALI_RECORDER_FILENAME=samples-${RUN}-FullHBM.cali \
-        CALI_REPORT_FILENAME=samples-${RUN}-FullHBM.json \
+    CALI_RECORDER_FILENAME=${CALI_CONFIG_PROFILE}-${RUN}-FullHBM.cali \
         numactl --membind=1 ./XSBench -s $1 -l $2 
 }
 
-# warmup
+if [ $# -ne 2 ]
+then
+    echo "Usage: $0 <cali-config-profile-name>"
+    exit 1
+fi
+
+# Warmup
+CALI_CONFIG_PROFILE=""
 ./XSBench -s small -l 10000
+
+# Set cali config profile here
+CALI_CONFIG_PROFILE=$1
 
 for size in small large XL
 do
@@ -35,4 +31,4 @@ do
     done
 done
 
-cali-query -q "SELECT *,sum(time.duration),sum(libpfm.counter.MEM_UOPS_RETIRED:ALL_LOADS),sum(libpfm.counter.MEM_UOPS_RETIRED:L2_MISS_LOADS) GROUP BY annotation,problem_size,problem_lookups FORMAT JSON(quote-all) ORDER BY problem_size,problem_lookups ASC,DESC" *.cali > xsbench_ensemble.json
+cali-query -q "SELECT *,sum(time.duration),sum(libpfm.counter.MEM_UOPS_RETIRED:ALL_LOADS),sum(libpfm.counter.MEM_UOPS_RETIRED:L2_MISS_LOADS) GROUP BY annotation,problem_size,problem_lookups FORMAT JSON(quote-all) ORDER BY problem_size,problem_lookups ASC,DESC" ${CALI_CONFIG_PROFILE}*.cali > ensemble-${CALI_CONFIG_PROFILE}.json
